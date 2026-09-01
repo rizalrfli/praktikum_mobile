1e8560da15c359170357838447cac7cd7062b0c2
rizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-09-01 14:15:59 +0700

    Rework: prak_1

---
+++ week 1/praktikum_1/bin/praktikum_1.dart
@@ -0,0 +1,16 @@
+import 'package:praktikum_1/praktikum_1.dart' as praktikum_1;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${praktikum_1.calculate()}!');
+  var nama = 'Afrizal';
+  var umur = 20;
+  var tinggi = "170";
+  var alamat = "Banyuwangi";
+  var tbd = "$umur + ${int.parse(tinggi)}";
+
+  print("nama: $nama");
+  print("umur: $umur");
+  print("tinggi: $tinggi");
+  print("alamat: $alamat");
+  print("tbd: $tbd");
+}
